e69a3e2fd7bcc4d302439ea149c12e3d3d794e0e
schweisen <Arthur.swyns@hotmail.com>
2025-09-17 10:12:10 +0000

    oefening 1 update

--- microbit data importeren/spotify dataset/main.py
+++ microbit data importeren/spotify dataset/main.py
@@ -13,8 +13,9 @@ with open('meeste gestreamde liedjes.csv', encoding='utf-8') as file:
     dualipa_count = 0
     for row in reader:
             # We gaan ervan uit dat de artiest in de TWEEDE kolom staat (index 1).
-            # Controleer of de naam 'Drake' in die kolom voorkomt.
+            # Controleer of de naam 'Dua Lipa' in die kolom voorkomt.
             if 'Dua Lipa' in row[1]:
                 # Stap 4: Als dat zo is, tel er eentje bij op.
                 dualipa_count += 1
-print("aantal keer dat drake voorkomt in de hitlijst: "+ str(dualipa_count))
+print("aantal keer dat Dua Lipa voorkomt in de hitlijst: "+ str(dualipa_count))
+          
